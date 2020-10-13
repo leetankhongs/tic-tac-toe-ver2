b1bf2ef5356e7e1b8ac87bad14aeb3e5e1e1aaa1
LAPTOP-IKDU3139\ADMIN <hunglt5@vng.com.vn>
2020-10-13 20:40:45 +0700

    Updates

---
+++ precache-manifest.f4f8338caf6a66fb82b65a54489850a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a255be6637b49e7cdbcd7bd9b9777bb1",
+    "url": "/tic-tac-toe-ver2/index.html"
+  },
+  {
+    "revision": "44c0a27800b0d3e8a6ba",
+    "url": "/tic-tac-toe-ver2/static/css/main.f33a5e68.chunk.css"
+  },
+  {
+    "revision": "391a73c7ec99a234f33d",
+    "url": "/tic-tac-toe-ver2/static/js/2.d16c8304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-ver2/static/js/2.d16c8304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c0a27800b0d3e8a6ba",
+    "url": "/tic-tac-toe-ver2/static/js/main.e4204d26.chunk.js"
+  },
+  {
+    "revision": "8b9f324eb2ffe70a3df3",
+    "url": "/tic-tac-toe-ver2/static/js/runtime-main.1a73be80.js"
+  }
+]);
